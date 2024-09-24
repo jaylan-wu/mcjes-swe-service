f94d6c5d27b4ae2a2e8ef27022018fc92480bee9
Jaylan Wu <jaylan.wu@nyu.edu>
2024-09-24 15:41:25 -0400

    Initial commit

---
+++ server/endpoints.py
@@ -0,0 +1,71 @@
+"""
+This is the file containing all of the endpoints for our flask app.
+The endpoint called `endpoints` will return all available endpoints.
+"""
+# from http import HTTPStatus
+
+from flask import Flask  # , request
+from flask_restx import Resource, Api  # Namespace, fields
+from flask_cors import CORS
+
+# import werkzeug.exceptions as wz
+
+app = Flask(__name__)
+CORS(app)
+api = Api(app)
+
+ENDPOINT_EP = '/endpoints'
+ENDPOINT_RESP = 'Available endpoints'
+HELLO_EP = '/hello'
+HELLO_RESP = 'hello'
+TITLE_EP = '/title'
+TITLE_RESP = 'Title'
+TITLE = 'The Journal of API Technology'
+EDITOR_RESP = 'Editor'
+EDITOR = 'ejc369@nyu.edu'
+DATE_RESP = 'Date'
+DATE = '2024-09-24'
+
+
+@api.route(HELLO_EP)
+class HelloWorld(Resource):
+    """
+    The purpose of the HelloWorld class is to have a simple test to see if the
+    app is working at all.
+    """
+    def get(self):
+        """
+        A trivial endpoint to see if the server is running.
+        """
+        return {HELLO_RESP: 'world'}
+
+
+@api.route(ENDPOINT_EP)
+class Endpoints(Resource):
+    """
+    This class will serve as live, fetchable documentation of what endpoints
+    are available in the system.
+    """
+    def get(self):
+        """
+        The `get()` method will return a sorted list of available endpoints.
+        """
+        endpoints = sorted(rule.rule for rule in api.app.url_map.iter_rules())
+        return {"Available endpoints": endpoints}
+
+
+@api.route(TITLE_EP)
+class JournalTitle(Resource):
+    """
+    This class handles creating, reading, updating
+    and deleting the journal title.
+    """
+    def get(self):
+        """
+        Retrieve the journal title.
+        """
+        return {
+            TITLE_RESP: TITLE,
+            EDITOR_RESP: EDITOR,
+            DATE_RESP: DATE,
+        }
---
+++ server/tests/test_endpoints.py
@@ -0,0 +1,32 @@
+from http.client import (
+    BAD_REQUEST,
+    FORBIDDEN,
+    NOT_ACCEPTABLE,
+    NOT_FOUND,
+    OK,
+    SERVICE_UNAVAILABLE,
+)
+
+from unittest.mock import patch
+
+import pytest
+
+import server.endpoints as ep
+
+TEST_CLIENT = ep.app.test_client()
+
+
+def test_hello():
+    resp = TEST_CLIENT.get(ep.HELLO_EP)
+    resp_json = resp.get_json()
+    assert ep.HELLO_RESP in resp_json
+
+
+def test_title():
+    resp = TEST_CLIENT.get(ep.TITLE_EP)
+    print(f'{ep.TITLE_EP=}')
+    resp_json = resp.get_json()
+    print(f'{resp_json=}')
+    assert ep.TITLE_RESP in resp_json
+    assert isinstance(resp_json[ep.TITLE_RESP], str)
+    assert len(resp_json[ep.TITLE_RESP]) > 0
