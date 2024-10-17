366a5dd5165a729ef8d47b9c124df62bca3d4481
Jaylan Wu <jaylan.wu@nyu.edu>
2024-10-17 09:02:43 -0400

    create routes and responses modules (#2)

---
+++ server/routes.py
@@ -0,0 +1,6 @@
+# Define API endpoint routes in alphabetical order
+ENDPOINT_ROUTE = '/endpoints'
+HELLO_ROUTE = '/hello'
+JOURNAL_ROUTE = '/journal'
+PEOPLE_ROUTE = '/people'
+TITLE_ROUTE = '/title'
