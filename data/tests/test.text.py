fa12ef220a900db7ae0e15b102f88cb93a0ef165
eddyse7en <150835474+eddyse7en@users.noreply.github.com>
2024-10-18 00:01:52 -0400

    Updated test.text.py
    Added two new functions:
    test_read_one and test_read_one_not found

--- data/tests/test.text.py
+++ data/tests/test.text.py
@@ -5,4 +5,13 @@ def test_read():
     texts = txt.read()
     assert isinstance(texts, dict)
     for key in texts:
-        assert isinstance(key, str)
+        assert isinstance(key, str) 
+
+
+def test_read_one():
+    assert len(txt.read_one(txt.TEST_KEY)) > 0
+
+
+def test_read_one_not_found():
+    assert txt.read_one('Not a page key!') == {}
+
